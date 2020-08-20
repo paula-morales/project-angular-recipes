493ac6446647f6c00994e7e21208b56cc2af406f
paula-morales <pmpoclava@gmail.com>
2020-08-20 11:31:05 +0200

    new array of recipes in component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Recipe } from "../recipe.model";
 
 @Component({
-  selector: 'app-recipe-list',
-  templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  selector: "app-recipe-list",
+  templateUrl: "./recipe-list.component.html",
+  styleUrls: ["./recipe-list.component.css"],
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      "Recipe 1",
+      "This is the first recipe",
+      "https://cdn.pixabay.com/photo/2015/04/29/19/33/cookbook-746005_960_720.jpg"
+    ),
+    new Recipe(
+      "Recipe 2",
+      "This is the second recipe",
+      "https://cdn.pixabay.com/photo/2015/04/29/19/33/cookbook-746005_960_720.jpg"
+    ),
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
