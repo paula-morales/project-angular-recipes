0f1a5862c891a5b8e5fc4971747dc4024a7e8581
paula-morales <pmpoclava@gmail.com>
2020-08-21 17:26:25 +0200

    ingredients added to the recipe model

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -1,11 +1,20 @@
+import { Ingredient } from "../shared/ingredient.model";
+
 export class Recipe {
   public name: string;
   public description: string;
   public image: string;
+  public ingredients: Ingredient[];
 
-  constructor(name: string, desc: string, image: string) {
+  constructor(
+    name: string,
+    desc: string,
+    image: string,
+    ingredients: Ingredient[]
+  ) {
     this.name = name;
     this.description = desc;
     this.image = image;
+    this.ingredients = ingredients;
   }
 }
