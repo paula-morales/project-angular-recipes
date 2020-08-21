ffd9f67c867dd6bd8a3d086c869711aa8fb52b11
paula-morales <pmpoclava@gmail.com>
2020-08-21 17:43:03 +0200

    ingredients added to the recipe

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,5 +1,6 @@
 import { Recipe } from "./recipe.model";
 import { EventEmitter } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
 
 //it will manage our recipes
 export class RecipeService {
@@ -10,12 +11,14 @@ export class RecipeService {
     new Recipe(
       "Recipe 1",
       "This is the first recipe",
-      "https://cdn.pixabay.com/photo/2015/04/29/19/33/cookbook-746005_960_720.jpg"
+      "https://cdn.pixabay.com/photo/2015/04/29/19/33/cookbook-746005_960_720.jpg",
+      [new Ingredient("tomatoes", 5), new Ingredient("pasta", 1)]
     ),
     new Recipe(
       "Recipe 2",
-      "This is the second recipe",
-      "https://cdn.pixabay.com/photo/2015/04/29/19/33/cookbook-746005_960_720.jpg"
+      "This is the first recipe",
+      "https://cdn.pixabay.com/photo/2015/04/29/19/33/cookbook-746005_960_720.jpg",
+      [new Ingredient("tomatoes", 5), new Ingredient("pasta", 1)]
     ),
   ];
   getRecipes() {
