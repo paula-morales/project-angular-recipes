2a582157e0a56b19aa4ffe94f4001e0970d01121
paula-morales <pmpoclava@gmail.com>
2020-08-21 09:54:30 +0200

    local reference to add a new ingredient

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ElementRef,
+  ViewChild,
+  Output,
+  EventEmitter,
+} from "@angular/core";
+import { Ingredient } from "src/app/shared/ingredient.model";
 
 @Component({
-  selector: 'app-shopping-edit',
-  templateUrl: './shopping-edit.component.html',
-  styleUrls: ['./shopping-edit.component.css']
+  selector: "app-shopping-edit",
+  templateUrl: "./shopping-edit.component.html",
+  styleUrls: ["./shopping-edit.component.css"],
 })
 export class ShoppingEditComponent implements OnInit {
+  //  #nameInput local reference
+  //to access to the local reference, @viewChild
+  //the value of the input is in nameInputRef.nativeElement.value
+  @ViewChild("nameInput", { static: false }) nameInputRef: ElementRef;
+  @ViewChild("amountInput", { static: false }) amountInputRef: ElementRef;
 
-  constructor() { }
+  //to send the ingredient added to the parent component
+  //ingredientAdded event can be listened by the parent component
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
 
-  ngOnInit(): void {
+  constructor() {}
+
+  ngOnInit(): void {}
+  //I want to pass the input to the parent component who managed the array of ingredients
+  onAddItem() {
+    const ingName = this.nameInputRef.nativeElement.value;
+    const ingAmount = this.amountInputRef.nativeElement.value;
+    const newIngredient = new Ingredient(ingName, ingAmount);
+    //we emit the new ingredient
+    this.ingredientAdded.emit(newIngredient);
   }
-
 }
