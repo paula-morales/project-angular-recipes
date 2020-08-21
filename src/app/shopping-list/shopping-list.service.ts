37a3b97ff6e6f8e4e67b59a6dbb6afe8da11279c
paula-morales <pmpoclava@gmail.com>
2020-08-21 17:22:24 +0200

    shopping list service

---
+++ src/app/shopping-list/shopping-list.service.ts
@@ -0,0 +1,21 @@
+import { Ingredient } from "../shared/ingredient.model";
+import { EventEmitter } from "@angular/core";
+
+export class ShoppingListService {
+  //to inform that our ingredients were changed
+  ingredientsChanged = new EventEmitter<Ingredient[]>();
+  private ingredients: Ingredient[] = [
+    new Ingredient("Apples", 5),
+    new Ingredient("Pineapple", 1),
+  ];
+
+  getIngredients() {
+    return this.ingredients.slice();
+  }
+
+  addIngredient(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+    //emit the new array of ingredients
+    this.ingredientsChanged.emit(this.ingredients.slice());
+  }
+}
