cdf9c3a233b682f8c2d665ea09a10a8c38560490
paula-morales <pmpoclava@gmail.com>
2020-08-21 16:18:38 +0200

    service modified with an event emitter

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,7 +1,10 @@
 import { Recipe } from "./recipe.model";
+import { EventEmitter } from "@angular/core";
 
 //it will manage our recipes
 export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
+
   //you cannot get access from outside (private)
   private recipes: Recipe[] = [
     new Recipe(
