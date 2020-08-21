7132bc5619f42ea5fa59164e806934f835ecfe3a
paula-morales <pmpoclava@gmail.com>
2020-08-21 09:27:34 +0200

    @Input from parent component to display info

--- src/app/recipes/recipe-details/recipe-details.component.ts
+++ src/app/recipes/recipe-details/recipe-details.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from "@angular/core";
+import { Recipe } from "../recipe.model";
 
 @Component({
-  selector: 'app-recipe-details',
-  templateUrl: './recipe-details.component.html',
-  styleUrls: ['./recipe-details.component.css']
+  selector: "app-recipe-details",
+  templateUrl: "./recipe-details.component.html",
+  styleUrls: ["./recipe-details.component.css"],
 })
 export class RecipeDetailsComponent implements OnInit {
+  // input from the parent component [recipe]="selectedRecipe"
+  @Input() recipe: Recipe;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
