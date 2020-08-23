078e0d358e92f34a34bdfce1437a78b7024e3d68
paula-morales <pmpoclava@gmail.com>
2020-08-23 17:43:18 +0200

    improving the Service with Subjects(Observables)

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,13 +1,12 @@
 import { Recipe } from "./recipe.model";
-import { EventEmitter, Injectable } from "@angular/core";
+import { Injectable } from "@angular/core";
+
 import { Ingredient } from "../shared/ingredient.model";
 import { ShoppingListService } from "../shopping-list/shopping-list.service";
 
 @Injectable()
 //it will manage our recipes
 export class RecipeService {
-  recipeSelected = new EventEmitter<Recipe>();
-
   //you cannot get access from outside (private)
   private recipes: Recipe[] = [
     new Recipe(
