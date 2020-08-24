a30dd16d444c4f71158aacc7a019b86164ec035c
paula-morales <pmpoclava@gmail.com>
2020-08-24 12:03:47 +0200

    delete an item button

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -51,6 +51,11 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
     this.editMode = false;
   }
 
+  onDelete() {
+    this.shoppingListService.deleteIngredient(this.indexItem);
+    this.onClear();
+  }
+
   ngOnDestroy() {
     this.subscription.unsubscribe();
   }
